6730185d8eb55e0e3ad0367901ff22a65f9a57bb
s7rul <serraerik1@gmail.com>
2017-08-03 19:20:53 +0200

    changed from spaces to tabs

--- logic.c
+++ logic.c
@@ -3,79 +3,79 @@
 #include <stdio.h>
 
 void clean_table(int **table){
-    int x;
-    int y;
+	int x;
+	int y;
 
-    for (x = 0; x < 3; x++){
-        for (y = 0; y < 3; y++){
-            table[x][y] = 0;
-        }
-    }
+	for (x = 0; x < 3; x++){
+		for (y = 0; y < 3; y++){
+			table[x][y] = 0;
+		}
+	}
 }
 
 int test_win(int **table, int turn){
 
-    for (int x; x < 3; x++){
-        if (table[x][0] == turn && table[x][1] == turn && table[x][2] == turn){
-            return 1;
-        }
-    }
+	for (int x; x < 3; x++){
+		if (table[x][0] == turn && table[x][1] == turn && table[x][2] == turn){
+		return 1;
+		}
+	}
 
-    for (int y; y < 3; y++){
-        if (table[0][y] == turn && table[1][y] == turn && table[2][y] == turn){
-            return 1;
-        }
-    }
+	for (int y; y < 3; y++){
+		if (table[0][y] == turn && table[1][y] == turn && table[2][y] == turn){
+		return 1;
+		}
+	}
 
-    if (table[0][0] == turn && table[1][1] == turn && table[2][2] == turn){
-        return 1;
-    }
+	if (table[0][0] == turn && table[1][1] == turn && table[2][2] == turn){
+		return 1;
+	}
 
-    if (table[2][0] == turn && table[1][1] == turn && table[0][2] == turn){
-        return 1;
-    }
+	if (table[2][0] == turn && table[1][1] == turn && table[0][2] == turn){
+		return 1;
+	}
 
-    return 0;
+	return 0;
 }
 
 int chartoint(char input){
-    switch (input){
-        case('1'):
-            return 0;
-        case('2'):
-            return 1;
-        case('3'):
-            return 2;
+	switch (input){
+		case('1'):
+			return 0;
+		case('2'):
+			return 1;
+		case('3'):
+			return 2;
 
-        case('a'):
-            return 0;
-        case('b'):
-            return 1;
-        case('c'):
-            return 2;
-    }
+		case('a'):
+			return 0;
+		case('b'):
+			return 1;
+		case('c'):
+			return 2;
+	}
 }
 
 void inputfunc(int **table, int turn){
 
-    char * input = (char *) malloc(sizeof(input));
-    int x;
-    int y;
+	char * input = (char *) malloc(sizeof(input));
+	int x;
+	int y;
 
-    while(1 == 1){
-        printf("Place marker on: ");
-        scanf("%s", input);
+	while(1 == 1){
+		printf("Place marker on: ");
+		scanf("%s", input);
 
-        x = chartoint(input[0]);
-        y = chartoint(input[1]);
+		x = chartoint(input[0]);
+		y = chartoint(input[1]);
 
-        printf("x:%d y:%d\n", x, y);
+		printf("x:%d y:%d\n", x, y);
 
-        if (table[x][y] == 0){
-            table[x][y] = turn;
-            break;
-        }
-    }
+		if (table[x][y] == 0){
+			table[x][y] = turn;
+			break;
+		}
+	}
 
-    free(input);
+	free(input);
 }
--- main.c
+++ main.c
@@ -13,74 +13,74 @@ void print_turn(int turn);
 
 
 int two_player_loop(){
-    int **table = (int **) malloc(3 * sizeof(int));
-    table[0] = (int *) malloc(3 * sizeof(int));
-    table[1] = (int *) malloc(3 * sizeof(int));
-    table[2] = (int *) malloc(3 * sizeof(int));
+	int **table = (int **) malloc(3 * sizeof(int));
+	table[0] = (int *) malloc(3 * sizeof(int));
+	table[1] = (int *) malloc(3 * sizeof(int));
+	table[2] = (int *) malloc(3 * sizeof(int));
 
-    int win;
-    int turn;
+	int win;
+	int turn;
 
-    clean_table(table);
-    turn = 1;
-    win = 0;
-
-
-    while(win == 0){
-        print_turn(turn);
-        printf("interger turn:%d\n", turn);
-        print_table(table);
-        inputfunc(table, turn);
-        win = test_win(table, turn);
-
-        if (turn == 1){turn = 2;}
-        else if (turn == 2){turn = 1;}
-    }
+	clean_table(table);
+	turn = 1;
+	win = 0;
 
 
-    free(table[0]);
-    free(table[1]);
-    free(table[2]);
-    free(table);
+	while(win == 0){
+		print_turn(turn);
+		printf("interger turn:%d\n", turn);
+		print_table(table);
+		inputfunc(table, turn);
+		win = test_win(table, turn);
+
+		if (turn == 1){turn = 2;}
+		else if (turn == 2){turn = 1;}
+	}
+
+
+	free(table[0]);
+	free(table[1]);
+	free(table[2]);
+	free(table);
 }
 
 int one_player_loop(){
-    printf("###################\n");
-    printf("#      Under      #\n");
-    printf("#  construction!  #\n");
-    printf("###################\n");
+	printf("###################\n");
+	printf("#      Under      #\n");
+	printf("#  construction!  #\n");
+	printf("###################\n");
 }
 
 int menu(){
-    int * shoice = (int *) malloc(sizeof(int));
+	int * shoice = (int *) malloc(sizeof(int));
     
-    printf("###################\n");
-    printf("# 1 - two player  #\n");
-    printf("# 2 - one player  #\n");
-    printf("# 3 - quit        #\n");
-    printf("###################\n");
-    printf("Your shoice: ");
-    scanf("%d", shoice);
+	printf("###################\n");
+	printf("# 1 - two player  #\n");
+	printf("# 2 - one player  #\n");
+	printf("# 3 - quit        #\n");
+	printf("###################\n");
+	printf("Your shoice: ");
+	scanf("%d", shoice);
 
-    switch(*shoice){
-        case 1:
-            free(shoice);
-            two_player_loop();
-            break;
-        case 2:
-            free(shoice);
-            one_player_loop();
-            break;
-        case 3:
-            free(shoice);
-            return 0;
-            break;
-    }
+	switch(*shoice){
+		case 1:
+			free(shoice);
+			two_player_loop();
+			break;
+		case 2:
+			free(shoice);
+			one_player_loop();
+			break;
+		case 3:
+			free(shoice);
+			return 0;
+			break;
+	}
 
-    menu();
+	menu();
 }
 
 int main(){
-    printf("Hello and wealcome to tic tac toe writhen in c.\n");
-    menu();
+	printf("Hello and wealcome to tic tac toe writhen in c.\n");
+	menu();
 }
